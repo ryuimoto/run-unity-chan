733dc12d08c8d72ce8111a8e20d9d72901bfa263
maca <narrow617@gmail.com>
2022-05-24 00:17:21 +0900

    e text

--- Assets/Scripts/GoalManager.cs
+++ Assets/Scripts/GoalManager.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GoalManager : MonoBehaviour
 {
     public GameObject player;
     public GameObject text;
+
+    private bool isGoal = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +21,26 @@ public class GoalManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGoal && Input.GetMouseButton(0))
+        {
+            Restart();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == player.name)
         {
-            text.GetComponent<TextMeshProUGUI>().text = "Goal!!!";
+            text.GetComponent<TextMeshProUGUI>().text = "Goal!!!\n画面クリックで再スタート";
             text.SetActive(true);
+
+            isGoal = true;
         }
+    }
+
+    private void Restart()
+    {
+        Scene loadScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadScene.name);
     }
 }
