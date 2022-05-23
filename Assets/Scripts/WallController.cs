23784ad478e9cd2376660612d1dc100c79dadb12
maca <narrow617@gmail.com>
2022-05-24 00:28:46 +0900

    add moving wall

--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class WallController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float speed = 0.05f;
+    public float max_x = 10.0f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.gameObject.transform.Translate(speed, 0, 0);
+
+        if (this.gameObject.transform.position.x > max_x || this.gameObject.transform.position.x < (-max_x))
+        {
+            speed *= -1;
+        }
     }
 }
